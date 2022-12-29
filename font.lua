7b855663681115cead918cd0795d05d907a6de83
Bug <77031032+0zBug@users.noreply.github.com>
2022-12-29 03:04:03 -0600

    Create font.lua

---
+++ font.lua
@@ -0,0 +1,524 @@
+return {
+	["A"] = {
+		{
+			Vector2.new(5, 0),
+			Vector2.new(1, 9),
+		},
+		{
+			Vector2.new(5, 0),
+			Vector2.new(9, 9),
+		},
+		{
+			Vector2.new(3, 6),
+			Vector2.new(7, 6),
+		}
+	},
+	["B"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(2, 0),
+			Vector2.new(6, 0),
+			Vector2.new(8, 1),
+			Vector2.new(8, 3),
+			Vector2.new(6, 4),
+		},
+		{
+			Vector2.new(2, 4),
+			Vector2.new(6, 4),
+			Vector2.new(8, 5),
+			Vector2.new(8, 8),
+			Vector2.new(6, 9),
+			Vector2.new(2, 9),
+		}
+	},
+	["C"] = {
+		{
+			Vector2.new(10, 1),
+			Vector2.new(8, 0),
+			Vector2.new(6, 0),
+			Vector2.new(4, 1),
+			Vector2.new(3, 3),
+			Vector2.new(3, 6),
+			Vector2.new(4, 8),
+			Vector2.new(6, 9),
+			Vector2.new(8, 9),
+			Vector2.new(10, 8),
+		}
+	},
+	["D"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(2, 0),
+			Vector2.new(5, 0),
+			Vector2.new(7, 1),
+			Vector2.new(8, 3),
+			Vector2.new(8, 6),
+			Vector2.new(7, 8),
+			Vector2.new(5, 9),
+			Vector2.new(2, 9),
+		}
+	},
+	["E"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(2, 0),
+			Vector2.new(8, 0),
+		},
+		{
+			Vector2.new(2, 4),
+			Vector2.new(6, 4),
+		},
+		{
+			Vector2.new(2, 9),
+			Vector2.new(8, 9),
+		}
+	},
+	["F"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(2, 0),
+			Vector2.new(8, 0),
+		},
+		{
+			Vector2.new(2, 4),
+			Vector2.new(6, 4),
+		}
+	},
+	["G"] = {
+		{
+			Vector2.new(10, 1),
+			Vector2.new(8, 0),
+			Vector2.new(6, 0),
+			Vector2.new(4, 1),
+			Vector2.new(3, 3),
+			Vector2.new(3, 6),
+			Vector2.new(4, 8),
+			Vector2.new(6, 9),
+			Vector2.new(8, 9),
+			Vector2.new(10, 8),
+			Vector2.new(10, 5),
+		},
+		{
+			Vector2.new(7, 5),
+			Vector2.new(10, 5),
+		}
+	},
+	["H"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(8, 0),
+			Vector2.new(8, 9),
+		},
+		{
+			Vector2.new(2, 4),
+			Vector2.new(8, 4),
+		}
+	},
+	["I"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		}
+	},
+	["J"] = {
+		{
+			Vector2.new(5, 0),
+			Vector2.new(5, 7),
+			Vector2.new(4, 9),
+			Vector2.new(2, 9),
+			Vector2.new(1, 7),
+			Vector2.new(1, 6),
+		}
+	},
+	["K"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(8, 0),
+			Vector2.new(2, 6),
+		},
+		{
+			Vector2.new(4, 4),
+			Vector2.new(8, 9),
+		}
+	},
+	["L"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(2, 9),
+			Vector2.new(8, 9),
+		}
+	},
+	["M"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(2, 0),
+			Vector2.new(6, 9),
+		},
+		{
+			Vector2.new(10, 0),
+			Vector2.new(6, 9),
+		},
+		{
+			Vector2.new(10, 0),
+			Vector2.new(10, 9),
+		}
+	},
+	["N"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(2, 0),
+			Vector2.new(8, 9),
+		},
+		{
+			Vector2.new(8, 0),
+			Vector2.new(8, 9),
+		}
+	},
+	["O"] = {
+		{
+			Vector2.new(6, 0),
+			Vector2.new(4, 1),
+			Vector2.new(3, 3),
+			Vector2.new(3, 6),
+			Vector2.new(4, 8),
+			Vector2.new(6, 9),
+			Vector2.new(7, 9),
+			Vector2.new(9, 8),
+			Vector2.new(10, 6),
+			Vector2.new(10, 3),
+			Vector2.new(9, 1),
+			Vector2.new(7, 0),
+			Vector2.new(6, 0),
+		}
+	},
+	["P"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(2, 0),
+			Vector2.new(6, 0),
+			Vector2.new(8, 1),
+			Vector2.new(8, 4),
+			Vector2.new(6, 5),
+			Vector2.new(2, 5),
+		}
+	},
+	["Q"] = {
+		{
+			Vector2.new(6, 0),
+			Vector2.new(4, 1),
+			Vector2.new(3, 3),
+			Vector2.new(3, 6),
+			Vector2.new(4, 8),
+			Vector2.new(6, 9),
+			Vector2.new(7, 9),
+			Vector2.new(9, 8),
+			Vector2.new(10, 6),
+			Vector2.new(10, 3),
+			Vector2.new(9, 1),
+			Vector2.new(7, 0),
+			Vector2.new(6, 0),
+		},
+		{
+			Vector2.new(7, 7),
+			Vector2.new(10, 10),
+		}
+	},
+	["R"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(2, 0),
+			Vector2.new(6, 0),
+			Vector2.new(8, 1),
+			Vector2.new(8, 4),
+			Vector2.new(6, 5),
+			Vector2.new(2, 5),
+		},
+		{
+			Vector2.new(5, 5),
+			Vector2.new(8, 9),
+		}
+	},
+	["S"] = {
+		{
+			Vector2.new(8, 1),
+			Vector2.new(6, 0),
+			Vector2.new(4, 0),
+			Vector2.new(2, 1),
+			Vector2.new(2, 2),
+			Vector2.new(3, 3),
+			Vector2.new(7, 5),
+			Vector2.new(8, 6),
+			Vector2.new(8, 8),
+			Vector2.new(6, 9),
+			Vector2.new(4, 9),
+			Vector2.new(2, 8),
+		}
+	},
+	["T"] = {
+		{
+			Vector2.new(5, 0),
+			Vector2.new(5, 9),
+		},
+		{
+			Vector2.new(1, 0),
+			Vector2.new(9, 0),
+		}
+	},
+	["U"] = {
+		{
+			Vector2.new(3, 0),
+			Vector2.new(3, 6),
+			Vector2.new(4, 8),
+			Vector2.new(6, 9),
+			Vector2.new(7, 9),
+			Vector2.new(9, 8),
+			Vector2.new(10, 6),
+			Vector2.new(10, 0),
+		}
+	},
+	["V"] = {
+		{
+			Vector2.new(1, 0),
+			Vector2.new(5, 9),
+		},
+		{
+			Vector2.new(9, 0),
+			Vector2.new(5, 9),
+		}
+	},
+	["W"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(4, 9),
+		},
+		{
+			Vector2.new(6, 0),
+			Vector2.new(4, 9),
+		},
+		{
+			Vector2.new(6, 0),
+			Vector2.new(8, 9),
+		},
+		{
+			Vector2.new(10, 0),
+			Vector2.new(8, 9),
+		}
+	},
+	["X"] = {
+		{
+			Vector2.new(2, 0),
+			Vector2.new(8, 9),
+		},
+		{
+			Vector2.new(8, 0),
+			Vector2.new(2, 9),
+		}
+	},
+	["Y"] = {
+		{
+			Vector2.new(1, 0),
+			Vector2.new(5, 4),
+			Vector2.new(5, 9),
+		},
+		{
+			Vector2.new(9, 0),
+			Vector2.new(5, 4),
+		}
+	},
+	["Z"] = {
+		{
+			Vector2.new(8, 0),
+			Vector2.new(2, 9),
+		},
+		{
+			Vector2.new(2, 0),
+			Vector2.new(8, 0),
+		},
+		{
+			Vector2.new(2, 9),
+			Vector2.new(8, 9),
+		}
+	},
+	["0"] = {
+		{
+			Vector2.new(5, 0),
+			Vector2.new(3, 1),
+			Vector2.new(2, 3),
+			Vector2.new(2, 6),
+			Vector2.new(3, 8),
+			Vector2.new(5, 9),
+			Vector2.new(7, 8),
+			Vector2.new(8, 6),
+			Vector2.new(8, 3),
+			Vector2.new(7, 1),
+			Vector2.new(5, 0),
+		}
+	},
+	["1"] = {
+		{
+			Vector2.new(3, 2),
+			Vector2.new(5, 0),
+			Vector2.new(5, 9),
+		}
+	},
+	["2"] = {
+		{
+			Vector2.new(2, 1),
+			Vector2.new(4, 0),
+			Vector2.new(6, 0),
+			Vector2.new(8, 1),
+			Vector2.new(8, 3),
+			Vector2.new(7, 5),
+			Vector2.new(2, 9),
+			Vector2.new(8, 9),
+		}
+	},
+	["3"] = {
+		{
+			Vector2.new(2, 1),
+			Vector2.new(4, 0),
+			Vector2.new(6, 0),
+			Vector2.new(8, 1),
+			Vector2.new(8, 3),
+			Vector2.new(6, 4),
+		},
+		{
+			Vector2.new(5, 4),
+			Vector2.new(6, 4),
+			Vector2.new(8, 5),
+			Vector2.new(8, 8),
+			Vector2.new(6, 9),
+			Vector2.new(4, 9),
+			Vector2.new(2, 8),
+		}
+	},
+	["4"] = {
+		{
+			Vector2.new(6, 0),
+			Vector2.new(6, 9),
+		},
+		{
+			Vector2.new(6, 0),
+			Vector2.new(1, 6),
+			Vector2.new(9, 6),
+		}
+	},
+	["5"] = {
+		{
+			Vector2.new(3, 0),
+			Vector2.new(2, 4),
+			Vector2.new(4, 3),
+			Vector2.new(5, 3),
+			Vector2.new(7, 4),
+			Vector2.new(8, 6),
+			Vector2.new(7, 8),
+			Vector2.new(5, 9),
+			Vector2.new(4, 9),
+			Vector2.new(2, 8),
+		},
+		{
+			Vector2.new(3, 0),
+			Vector2.new(7, 0),
+		}
+	},
+	["6"] = {
+		{
+			Vector2.new(7, 0),
+			Vector2.new(5, 0),
+			Vector2.new(3, 1),
+			Vector2.new(2, 3),
+			Vector2.new(2, 6),
+			Vector2.new(3, 8),
+			Vector2.new(5, 9),
+			Vector2.new(7, 8),
+			Vector2.new(8, 6),
+			Vector2.new(7, 4),
+			Vector2.new(5, 3),
+			Vector2.new(3, 4),
+			Vector2.new(2, 6),
+		}
+	},
+	["7"] = {
+		{
+			Vector2.new(8, 0),
+			Vector2.new(4, 9),
+		},
+		{
+			Vector2.new(2, 0),
+			Vector2.new(8, 0),
+		}
+	},
+	["8"] = {
+		{
+			Vector2.new(4, 0),
+			Vector2.new(2, 1),
+			Vector2.new(2, 3),
+			Vector2.new(4, 4),
+			Vector2.new(6, 4),
+			Vector2.new(8, 3),
+			Vector2.new(8, 1),
+			Vector2.new(6, 0),
+			Vector2.new(4, 0),
+		},
+		{
+			Vector2.new(4, 4),
+			Vector2.new(2, 5),
+			Vector2.new(2, 8),
+			Vector2.new(4, 9),
+			Vector2.new(6, 9),
+			Vector2.new(8, 8),
+			Vector2.new(8, 5),
+			Vector2.new(6, 4),
+		}
+	},
+	["9"] = {
+		{
+			Vector2.new(8, 3),
+			Vector2.new(7, 5),
+			Vector2.new(5, 6),
+			Vector2.new(3, 5),
+			Vector2.new(2, 3),
+			Vector2.new(3, 1),
+			Vector2.new(5, 0),
+			Vector2.new(7, 1),
+			Vector2.new(8, 3),
+			Vector2.new(8, 6),
+			Vector2.new(7, 8),
+			Vector2.new(5, 9),
+			Vector2.new(3, 9),
+		}
+	}
+}
